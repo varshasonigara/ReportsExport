fe874a53b02223ec6235b63dddec918e1ecf5e5e
varshasonigara <varshasonigara2017@gmail.com>
2018-09-07 12:43:56 +0530

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+declare const $;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {
+    $(function () {
+      $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+    });
+  }
+}
