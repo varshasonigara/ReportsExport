4b264a03fa40bce46aa0301cc8d1fe2f3bae9471
varshasonigara <varshasonigara2017@gmail.com>
2018-09-07 19:24:14 +0530

    style exportButtons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { data } from './data';
 declare const $;
 
 @Component({
@@ -8,13 +9,39 @@ declare const $;
 })
 export class AppComponent implements OnInit {
   constructor() {}
+  data:any=[];
   ngOnInit() {
+    this.data=data;
     $(function () {
       $('#example').DataTable( {
         dom: 'Bfrtip',
+        searching: false,
         buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
+          {
+              extend:    'copyHtml5',
+              text:      '<img src="../assets/icons/copy.png"/>',
+              titleAttr: 'Copy',
+              className: 'exportButton'
+          },
+          {
+              extend:    'excelHtml5',
+              text:      '<img src="../assets/icons/excel.png"/>',
+              titleAttr: 'Excel',
+              className: 'exportButton'
+          },
+          {
+              extend:    'csvHtml5',
+              text:      '<img src="../assets/icons/csv.png"/>',
+              titleAttr: 'CSV',
+              className: 'exportButton'
+          },
+          {
+              extend:    'pdfHtml5',
+              text:      '<img src="../assets/icons/pdf.png"/>',
+              titleAttr: 'PDF',
+              className: 'exportButton'
+          }
+      ]
     } );
     });
   }
---
+++ src/app/data.ts
@@ -0,0 +1,678 @@
+export const data = [
+    {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    },
+    {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }, {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320,800"
+    }
+];
+
+
